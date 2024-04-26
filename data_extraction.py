b81ad0098ca6b0212d3efe92f95013cae9917b67
shockorjoke <73474627+shockorjoke@users.noreply.github.com>
2024-04-26 11:46:52 +0000

    apply filter

--- data_extraction.py
+++ data_extraction.py
@@ -11,37 +11,46 @@ dataset = load_dataset("bookcorpus")
 nlp = spacy.load("en_core_web_trf")
 """Extracts the sentences of a noun from Brown corpus. Saves the sentences as a list of str.
 
-Your Tasks:
-- Write the method extract_noun_sentences()
 """
+#with open(f"selected 100 sents.txt", "r") as ds:
+#    dataset = ds.readlines()
+
 #@jit(target_backend='cuda') 
 def extract_noun_sentences(target_nouns, dataset):
     """Extract the sentences of a noun from Brown corpus.
     
     Returns:
     list: sentences (str) that contain the target noun."""
-    
+    #Edit between here
     sentence_with_directed_noun = {}
+    seen = []
+    count=0
     for split in dataset.keys():
         data = dataset[split]
         #use this for small data
         sentences = data["text"]
         #sentences = data["text"]
         for sentence in sentences:
+            count+=1
+            if count%1000000 == 0:
+                print(count)
             for target_noun in target_nouns:
                 if target_noun in sentence:
-                    doc = nlp(sentence)
-                    for token in doc:
-                        if token.text == target_noun and token.dep_ == "dobj":
-                            if target_noun not in sentence_with_directed_noun:
-                                sentence_with_directed_noun[target_noun]=[sentence]
-                            else:
-                                if sentence not in sentence_with_directed_noun[target_noun]: #this statement was insert because it seem that there are some sentences which appear 2 time in corpus
+                    if sentence not in seen:
+                        doc = nlp(sentence)
+                        for token in doc:
+                            if token.text == target_noun and token.dep_ == "dobj":
+                                seen.append(sentence)
+                                if target_noun not in sentence_with_directed_noun:
+                                    sentence_with_directed_noun[target_noun]=[sentence]
+                                else:
                                     sentence_with_directed_noun[target_noun].append(sentence)
+                    else: pass
+                else: pass
     return sentence_with_directed_noun
 
-    
 target_nouns = ["dinner","breakfast","feast","meal","buffet","speech","response","submission","lecture","conversation","brochure","textbook","novel","diary","summary","boat","classroom","cave","apartment","tower"]
+
 
 
 
@@ -51,4 +60,4 @@ for target_noun, sentences in sentence_with_directed_noun.items():
     if len(sentences)>0:
         with open(f"sentences/{target_noun}.pickle", "wb") as sentence_file:
             pickle.dump(sentences, sentence_file)
-print("Time: ", timer()-start)
+print("with GPU:", timer()-start)
